9c07c29a0ca833468019334a7548769d6ad2cd47
JohnMillimetron <lozhkin.2005@bk,ru>
2020-12-06 14:18:11 +0400

    Ноты отображаются во всех 16 каналах, удаление не работает, в остальном всё ок. Инструмент по-прежнему задаётся из единого поля.

--- MIDIMozartGraphic.py
+++ MIDIMozartGraphic.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QHBoxLayout, QSizePolicy
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QHBoxLayout, QSizePolicy, QWidget, QFrame
 from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtCore import QCoreApplication, Qt, QRect
 from MIDIMozartClasses import Composition, NOTES_AND_NAMES_is, NOTE_NUMBERS_AND_OCTAVES
@@ -36,20 +36,39 @@ class MainWindow(QMainWindow):
         while temp:
             eval(f'self.duration_button_{temp.pop()}.clicked.connect(self.duration_button_clicked)')
 
+        # Создание лэйаутов
+        self.chanel_layouts = [QHBoxLayout(self.chanel1notes_frame), QHBoxLayout(self.chanel2notes_frame),
+                               QHBoxLayout(self.chanel3notes_frame), QHBoxLayout(self.chanel4notes_frame),
+                               QHBoxLayout(self.chanel5notes_frame), QHBoxLayout(self.chanel6notes_frame),
+                               QHBoxLayout(self.chanel7notes_frame), QHBoxLayout(self.chanel8notes_frame),
+                               QHBoxLayout(self.chanel9notes_frame), QHBoxLayout(self.chanel10notes_frame),
+                               QHBoxLayout(self.chanel11notes_frame), QHBoxLayout(self.chanel12notes_frame),
+                               QHBoxLayout(self.chanel13notes_frame), QHBoxLayout(self.chanel14notes_frame),
+                               QHBoxLayout(self.chanel15notes_frame), QHBoxLayout(self.chanel16notes_frame)]
+        for i in range(16):
+            eval(f'self.chanel_layouts[{i}].setGeometry(QRect(161, 0, 3840, 80))')
+            eval(f'self.chanel_layouts[{i}].setSpacing(0)')
+            eval(f'self.chanel_layouts[{i}].setAlignment(Qt.AlignLeft)')
+            eval(f'self.chanel_layouts[{i}].setContentsMargins(0, 0, 0, 0)')
+            # self.chanel_layouts[i].addWidget(QPushButton())
+
+        # Привязка кнопок к обработчикам
         self.create_button.clicked.connect(self.create_midi)
         self.instrument_input.valueChanged.connect(self.instrument_change)
 
-        self.layout1 = QHBoxLayout(self.chanel1notes_frame)
-        self.layout1.setGeometry(QRect(161, 0, 3840, 80))
-        self.layout1.setSpacing(0)
-        self.layout1.setAlignment(Qt.AlignLeft)
-        self.layout1.setContentsMargins(0, 0, 0, 0)
-
     def key_clicked(self):
-        MyComposition[int(self.chanel_input.value())].add_note(pitch=int(self.sender().text().split('\n')[1]),
-                                                               duration=float(self.current_duration))
+        MyComposition[int(self.chanel_input.value()) - 1].add_note(pitch=int(self.sender().text().split('\n')[1]),
+                                                                   duration=float(self.current_duration))
         self.make_button(text=self.sender().text().split('\n')[0], x=int(self.current_duration * 100),
                          ch_n=int(self.chanel_input.value()))
+
+    def make_button(self, x, text, ch_n):
+        btn = QPushButton(text=f'{ch_n}\n{len(MyComposition[ch_n - 1].notes)}\n{text}')
+        btn.setMaximumWidth(x)
+        btn.setMinimumWidth(x)
+        btn.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Minimum)
+        btn.clicked.connect(self.delete_note)
+        eval(f'self.chanel_layouts[{ch_n - 1}].addWidget(btn)')
 
     def duration_button_clicked(self):
         temp = {"1": 4, "2": 2, "2.": 3, "4": 1, "4.": 1.5, "8": 0.5, "8.": 0.75, "16": 0.25, "16.": 0.375}
@@ -83,25 +102,16 @@ class MainWindow(QMainWindow):
             self.H4.click()
 
     def instrument_change(self, instr_num):
-        MyComposition[int(self.chanel_input.value())].set_instrument(int(instr_num) - 1)
+        MyComposition[int(self.chanel_input.value()) - 1].set_instrument(int(instr_num) - 1)
 
     def create_midi(self):
         print(MyComposition)
         MyComposition.export_as_midi('test4.mid')
 
-    def make_button(self, x, text, ch_n):
-        btn = QPushButton(text=f'{ch_n}\n{len(MyComposition[ch_n].notes) - 1}\n{text}')
-        btn.setMaximumWidth(x)
-        btn.setMinimumWidth(x)
-        btn.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Minimum)
-        btn.clicked.connect(self.delete_note)
-        self.layout1.addWidget(btn)
-
     def delete_note(self):
         ch_n, note_n, _ = self.sender().text().split('\n')
-        MyComposition[int(ch_n)].remove_note(int(note_n))
-        self.layout1.removeWidget(self.sender())
-        self.layout1.update()
+        MyComposition[int(ch_n) - 1].remove_note(int(note_n) - 1)
+        self.chanel_layouts[ch_n - 1].removeWidget(self.sender())
 
     # def refresh(self):
     #     for i, chanel in enumerate(MyComposition):
